44b59dadc8f1ee5e5bb794f377b626ead2f911ec
Krit B <krit@screencloud.io>
2022-05-08 01:07:50 +0700

    fix(CI): cleanup and fix CI

--- src/components/IsometricGrid/index.tsx
+++ src/components/IsometricGrid/index.tsx
@@ -1,25 +1,8 @@
 import React from "react";
-import Tile from "../Tile";
-import { IGridProps, IRowProps } from "./types";
+import { IGridProps } from "./types";
 import IsometricPlane from "../IsometricPlane";
 import Grid from "../Grid";
 import "./styles.css";
-
-const Row: React.FunctionComponent<IRowProps> = ({
-  blockSize,
-  numOfCol,
-  children,
-}) => {
-  const width = blockSize * numOfCol;
-  return (
-    <div
-      className="row"
-      style={{ width: `${width}px`, height: `${blockSize}px` }}
-    >
-      {children}
-    </div>
-  );
-};
 
 const IsometricGrid: React.FunctionComponent<IGridProps> = ({
   opts,
@@ -28,26 +11,6 @@ const IsometricGrid: React.FunctionComponent<IGridProps> = ({
   onTileClick,
   renderTile,
 }) => {
-  const { numOfRow, numOfCol, blockSize } = opts;
-  const generateGrid = () => {
-    return Array(numOfRow)
-      .fill("r")
-      .map((_, rIndex) => (
-        <Row key={rIndex} blockSize={blockSize} numOfCol={numOfCol}>
-          {Array(numOfCol)
-            .fill("t")
-            .map((_, tIndex) => (
-              <Tile
-                key={tIndex}
-                index={tIndex + rIndex * numOfCol}
-                onTileClick={onTileClick}
-                onTileHover={onTileHover}
-                render={renderTile}
-              />
-            ))}
-        </Row>
-      ));
-  };
   return (
     <IsometricPlane>
       <div className={className}>

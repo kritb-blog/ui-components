a69fd47f5487217ac7fa90e0a5b2a195bdf77e5b
Krit B <krit@screencloud.io>
2022-05-08 14:16:46 +0700

    fix(styled-components): issue from multiple instance

--- src/stories/Badge.stories.tsx
+++ src/stories/Badge.stories.tsx
@@ -3,6 +3,8 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Badge } from "..";
 import Github from "./assets/github.svg";
 import { IconSize } from "../theming/sizes";
+import { ThemeProvider } from "styled-components";
+import { Colors } from "../theming/colors";
 
 export default {
   title: "Example/Badge",
@@ -12,12 +14,16 @@ export default {
   },
 } as ComponentMeta<typeof Badge>;
 
-const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />;
+const Template: ComponentStory<typeof Badge> = (args) => (
+  <ThemeProvider theme={{ primary: Colors.Primary, active: Colors.Active }}>
+    <Badge {...args} />
+  </ThemeProvider>
+);
 
 export const Default = Template.bind({});
 Default.args = {
   badgeImageUrl: Github,
   badgeName: "Github",
   targetUrl: "https://www.github.com",
-  size: IconSize.M
+  size: IconSize.M,
 };
